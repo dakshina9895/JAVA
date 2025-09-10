a07b34a40d49bfe058d1318cd439ca6b31d6b2f5
dakshina9895 <162657480+dakshina9895@users.noreply.github.com>
2025-09-10 22:36:58 +0530

    Update 01_Student.java

--- week_1/Classes and Objects/01_Student.java
+++ week_1/Classes and Objects/01_Student.java
@@ -1,1 +1,30 @@
 
+# 1. Create a student class with fields: name, ID, and marks. Include methods to input and display details.
+
+class Student1
+{
+	private String name;
+	private int ID;
+	private double marks;
+	
+	public Student1(String n, int i, double m) {
+		this.name = n;
+		this.ID = i;
+		this.marks = m;
+	}
+	
+	public void display()
+	{
+		System.out.println("Name : " + this.name);
+		System.out.println("ID : " + this.ID);
+		System.out.println("Marks : " + this.marks);
+	}
+}
+public class Student {
+
+	public static void main(String[] args) {
+		Student1 s1 = new Student1("Ishika", 60, 89.8);
+		s1.display();
+		
+	}
+}

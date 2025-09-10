fddb6b4f2402e1a889af012ae0f3d5e1479c8d07
dakshina9895 <162657480+dakshina9895@users.noreply.github.com>
2025-09-10 22:37:49 +0530

    Update 01_Abstract.java

--- week_1/Abstraction and Encapsulation/01_Abstract.java
+++ week_1/Abstraction and Encapsulation/01_Abstract.java
@@ -1,1 +1,29 @@
+# 1. Create an abstract class Appliance with abstract method start(). Create concrete classes Fan and WashingMachine to implement it.
 
+  abstract class Appliance {
+    abstract void start();
+}
+
+class Fan extends Appliance {
+    @Override
+    void start() {
+        System.out.println("Fan is starting... Spinning blades!");
+    }
+}
+
+class WashingMachine extends Appliance {
+    @Override
+    void start() {
+        System.out.println("Washing Machine is starting... Washing clothes!");
+    }
+}
+
+public class AbstractClassDemo {
+    public static void main(String[] args) {
+        Appliance a1 = new Fan();
+        Appliance a2 = new WashingMachine();
+
+        a1.start();
+        a2.start();
+    }
+}

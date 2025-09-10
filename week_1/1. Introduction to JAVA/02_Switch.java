4842ce7d00651f57dd7929b5ee914f63465eef9d
dakshina9895 <162657480+dakshina9895@users.noreply.github.com>
2025-09-10 22:38:53 +0530

    Update 02_Switch.java

--- week_1/1. Introduction to JAVA/02_Switch.java
+++ week_1/1. Introduction to JAVA/02_Switch.java
@@ -1,1 +1,44 @@
 
+# 2. Write a program using switch-case to perform basic arithmetic operations: addition, subtraction, multiplication, and division.
+
+import java.util.Scanner;
+
+public class Switch {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		double num1 = sc.nextDouble();
+		
+		System.out.println("Enter second number :");
+		double num2 = sc.nextDouble();
+		
+		System.out.println("Enter the operation you want to perform (+,-,*,/) :");
+		String op = sc.next();
+		
+		switch(op) {
+		case "+":
+			double sum = num1 + num2;
+			System.out.println("Addition : "+ sum);
+			break;
+		
+		case "-":
+			double sub = num1 - num2;
+			System.out.println("Subtraction : "+ sub);
+			break;
+			
+		case "*":
+			double mult = num1 * num2;
+			System.out.println("Multiplication : "+ mult);
+			break;
+			
+		case "/":
+			double div = num1 / num2;
+			System.out.println("Division : "+ div);
+			break;
+			
+		default:
+			System.out.println("Invalid operator.");
+		}		
+	}
+}
+

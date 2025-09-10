91d080f22d2a99121a29a6a0c4e4c69077b6cd8f
dakshina9895 <162657480+dakshina9895@users.noreply.github.com>
2025-09-10 22:34:41 +0530

    Update 03_TwoPackages.java

--- week_1/Packages and Access Control/03_TwoPackages.java
+++ week_1/Packages and Access Control/03_TwoPackages.java
@@ -1,1 +1,24 @@
 
+# 3. Create two packages: package1 with class A and package2 with class B. Access class A from B using import.
+
+#Class A in package1
+package package1;
+
+public class A {
+    public void display() {
+        System.out.println("Hello from Class A (package1)");
+    }
+}
+
+#Class B in package2
+package package2;
+
+import package1.A;   // importing class A from package1
+
+public class B {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.display();
+    }
+}
+

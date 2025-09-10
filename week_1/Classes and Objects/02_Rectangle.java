e1ade4ec176724f3e9dd7638b68f8394abfbecc7
dakshina9895 <162657480+dakshina9895@users.noreply.github.com>
2025-09-10 22:37:14 +0530

    Update 02_Rectangle.java

--- week_1/Classes and Objects/02_Rectangle.java
+++ week_1/Classes and Objects/02_Rectangle.java
@@ -1,1 +1,36 @@
+# 2. Design a class Rectangle with length and breadth. Add a method to calculate area and perimeter.
 
+  class Rectangle1
+{
+	private double length;
+	private double breadth;
+	
+	public Rectangle1(double l, double b)
+	{
+		this.length =l;
+		this.breadth = b;
+	}
+	
+	public double area()
+	{
+		return this.length * this.breadth;
+	}
+	
+	public double perimeter()
+	{
+		return area() * 2;
+	}
+	
+	public void display()
+	{
+		System.out.println("Area : " + area());
+		System.out.println("Perimeter : " + perimeter());
+	}
+}
+public class Rectangle {
+
+	public static void main(String[] args) {
+		Rectangle1 r = new Rectangle1(4.5, 7.8);
+		r.display();
+	}
+}
